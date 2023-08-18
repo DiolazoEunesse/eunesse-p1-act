5e2c0427deade35225b194ac90a2b6b57b095c28
DiolazoEunesse <diolazoeuness3@gmail.com>
2023-08-18 20:45:18 +0800

    added social links

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const SocialLinks = ()=> {
   return(
     <div className="d-flex gap-2 p-2 fs-3">
-      <Link to="https://github.com/" className="text-dark">
+      <Link to="https://github.com/DiolazoEunesse" className="text-dark">
         <FaGithub />
       </Link>
-      <Link to="https://www.facebook.com/" className="text-dark">
+      <Link to="https://www.facebook.com/yoxeix" className="text-dark">
         <FaFacebook />
       </Link>
-      <Link to="https://instagram.com/" className="text-dark">
+      <Link to="https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2F000xi.0%3Futm_source%3Dqr%26igshid%3DMzNlNGNkZWQ4Mg%253D%253D%26fbclid%3DIwAR3PZayqUWf2R0tPaVPpdX8aaNH7wroBkN3O00hFwNMMf2fW0urfycIlEI0&h=AT3zXKD34S14o3fDQ0YVcrjUmcciMmi51lgGoxmxNDO7JxnPSjwt81Z1Nfzyfat5xL0ifGZaxlVmFLjJwPEDRg4HX_MplRnm0GGPCBZPW7yqzjIJe68B9BROmceNowS02F973g" className="text-dark">
         <FaInstagram />
       </Link>
     </div>
